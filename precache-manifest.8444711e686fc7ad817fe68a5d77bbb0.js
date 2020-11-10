a60941e81ac38b7d55ab706ffb2e1ba9c4e30e85
ednese <evancours94@gmail.com>
2020-11-10 05:02:39 +0100

    Updates

---
+++ precache-manifest.8444711e686fc7ad817fe68a5d77bbb0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d33cf55450a92cbc96090840650a414",
+    "url": "/HoffsonRedux/index.html"
+  },
+  {
+    "revision": "fbac2f248eed853f5c24",
+    "url": "/HoffsonRedux/static/css/main.f6b992f1.chunk.css"
+  },
+  {
+    "revision": "6e55014a7b0edf1b732b",
+    "url": "/HoffsonRedux/static/js/2.49200d1f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/HoffsonRedux/static/js/2.49200d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbac2f248eed853f5c24",
+    "url": "/HoffsonRedux/static/js/main.b3036823.chunk.js"
+  },
+  {
+    "revision": "019fdadaa888004c8963",
+    "url": "/HoffsonRedux/static/js/runtime-main.14ae12f1.js"
+  },
+  {
+    "revision": "59f1abcb0bd2a2f83141f5ccd3ba0242",
+    "url": "/HoffsonRedux/static/media/clock-home.59f1abcb.jpg"
+  },
+  {
+    "revision": "fb4a471bd43faf6c14b78f1a1665431a",
+    "url": "/HoffsonRedux/static/media/light-home.fb4a471b.jpg"
+  },
+  {
+    "revision": "3d4a8756a3367c73946a4dba2ed04da7",
+    "url": "/HoffsonRedux/static/media/living-room-filter-dark.3d4a8756.png"
+  },
+  {
+    "revision": "9cec676179a1f8ba8a52a7c8cc4abfe4",
+    "url": "/HoffsonRedux/static/media/logo.9cec6761.png"
+  },
+  {
+    "revision": "b2694bc36d081889c68a62db11110cc2",
+    "url": "/HoffsonRedux/static/media/shopping-bag-white.b2694bc3.svg"
+  },
+  {
+    "revision": "d7fffa9bc05ec4481da7bbe8270867cf",
+    "url": "/HoffsonRedux/static/media/vase-home.d7fffa9b.jpg"
+  }
+]);
